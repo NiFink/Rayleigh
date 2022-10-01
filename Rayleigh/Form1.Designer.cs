0c7fadf0004cbff34058440da47c640dca3a21cb
NiFink <Nils@team-fink.de>
2022-10-01 11:44:28 +0200

    Rayleigh

---
+++ Rayleigh/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Rayleigh
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lbTime = new System.Windows.Forms.Label();
+            this.lbDate = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbTime
+            // 
+            this.lbTime.AutoSize = true;
+            this.lbTime.BackColor = System.Drawing.Color.Transparent;
+            this.lbTime.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTime.Location = new System.Drawing.Point(152, 18);
+            this.lbTime.Name = "lbTime";
+            this.lbTime.Size = new System.Drawing.Size(127, 46);
+            this.lbTime.TabIndex = 0;
+            this.lbTime.Text = "Hallo";
+            // 
+            // lbDate
+            // 
+            this.lbDate.AutoSize = true;
+            this.lbDate.BackColor = System.Drawing.Color.Transparent;
+            this.lbDate.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbDate.Location = new System.Drawing.Point(151, 60);
+            this.lbDate.Name = "lbDate";
+            this.lbDate.Size = new System.Drawing.Size(440, 46);
+            this.lbDate.TabIndex = 1;
+            this.lbDate.Text = "------------------------------";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.pictureBox1.Location = new System.Drawing.Point(-48, -17);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(132, 665);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackgroundImage = global::Rayleigh.Properties.Resources.Earth;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(782, 633);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lbDate);
+            this.Controls.Add(this.lbTime);
+            this.ForeColor = System.Drawing.SystemColors.Control;
+            this.Name = "Form1";
+            this.Text = "Rayleigh";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbTime;
+        private System.Windows.Forms.Label lbDate;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Rayleigh/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+
+namespace Rayleigh
+{
+    public partial class Form1 : Form
+    {
+        SpeechRecognitionEngine r = new SpeechRecognitionEngine();
+        SpeechSynthesizer s = new SpeechSynthesizer();
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Choices commands = new Choices();
+            commands.Add(new string[] { "hallo", "wie geht es dir", "Mir geht es gut", "Wie viel Uhr ist es", "Welcher Tag ist heute" 
+            ,"Test" ,"Stopp"});
+
+            GrammarBuilder gbuilder = new GrammarBuilder();
+            gbuilder.Append(commands);
+
+            Grammar grammar = new Grammar(gbuilder);
+
+            r.LoadGrammar(grammar);
+            r.SetInputToDefaultAudioDevice();
+            r.SpeechRecognized += recEngine_SpeechRecognized;
+
+            r.RecognizeAsync(RecognizeMode.Multiple);
+            s.SelectVoiceByHints(VoiceGender.Male, VoiceAge.Adult);
+
+            s.SpeakAsync("Hallo, ich heiße Rayleigh. Es freut mich dich kennen zu lernen");
+
+        }
+        private void recEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            switch (e.Result.Text)
+            {
+                case "Rayleigh":
+                    s.SpeakAsync("Willkommen");
+                    break;
+                case "hallo":
+                    s.SpeakAsync("Hallo Nutzer");
+                    break;
+                case "wie geht es dir":
+                    s.SpeakAsync("Mir geht es Gut und wie geht es dir");
+                    break;
+                case "Mir geht es gut":
+                    s.SpeakAsync("Das freut mich");
+                    break;
+                case "Wie viel Uhr ist es":
+                    s.SpeakAsync("Es ist" + DateTime.Now.ToString("HH") + " Uhr " + DateTime.Now.ToString("mm"));
+                    break;
+                case "Welcher Tag ist heute":
+                    s.SpeakAsync("Heute ist der" + DateTime.Now.ToString("d"));
+                    break;
+                case "Test":
+                    s.SpeakAsync("Hallo Nutzer ich mache jetzt einen Testdurchlauf Test beginnt Jetzt");
+                    s.SpeakAsync("Mir geht es Gut und wie geht es dir");
+                    s.SpeakAsync("Es ist" + DateTime.Now.ToString("HH") + " Uhr " + DateTime.Now.ToString("mm"));
+                    s.SpeakAsync("Heute ist der" + DateTime.Now.ToString("d"));
+                    break;
+                case "Stopp":
+                    Application.Exit(); 
+                    break;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lbTime.Text = DateTime.Now.ToLongTimeString();
+            lbDate.Text = DateTime.Now.ToLongDateString();
+        }
+    }
+}
